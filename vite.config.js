377358653ad6ddd3084fa7f6e8a75baf76342bb1
jithendrababug <jithendrababug@gmail.com>
2026-02-20 11:33:43 +0530

    Fix certification

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === "production" ? "/Portfolio/" : "/", // ✅ prod = GitHub, dev = localhost
-}));
+  base: "/Portfolio/",
+});
